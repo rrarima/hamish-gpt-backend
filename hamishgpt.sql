7e95ee0c03aad0a4d2fb7c0e20ea6724527fda8c
rrarima <5708@holbertonstudents.com>
2023-06-20 02:45:15 +0000

    mysql fix attempt

--- hamishgpt.sql
+++ hamishgpt.sql
@@ -18,34 +18,6 @@ USE `hamishgpt`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `images`
---
-
-DROP TABLE IF EXISTS `images`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `images` (
-  `imageid` int NOT NULL AUTO_INCREMENT,
-  `userid` int DEFAULT NULL,
-  `image_url` varchar(255) NOT NULL,
-  `image_description` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`imageid`),
-  KEY `userid` (`userid`),
-  CONSTRAINT `images_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `images`
---
-
-LOCK TABLES `images` WRITE;
-/*!40000 ALTER TABLE `images` DISABLE KEYS */;
-INSERT INTO `images` VALUES (1,1,'https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQbxKU661kNI0m7Yw0xBW5SrN5Uc56_8cq4t4OhaYMXIvPhIgJo8v7H2d3PDtIDsfQt','A Girrafe'),(2,1,'https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRJpevBOQMgZ-Gaeb54y2Tin-o5vsoOtYW2nYtCpzkYqtj6ENWsHD4d1Bv-AUymOggA','A Lion'),(3,1,'https://upload.wikimedia.org/wikipedia/commons/3/3f/Walking_tiger_female.jpg','A Tiger'),(4,1,'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Southern_right_whale.jpg/220px-Southern_right_whale.jpg','A Whale'),(5,8,'https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRJpevBOQMgZ-Gaeb54y2Tin-o5vsoOtYW2nYtCpzkYqtj6ENWsHD4d1Bv-AUymOggA','User 8'),(6,8,'https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRJpevBOQMgZ-Gaeb54y2Tin-o5vsoOtYW2nYtCpzkYqtj6ENWsHD4d1Bv-AUymOggA','Id 8'),(7,8,'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Southern_right_whale.jpg/220px-Southern_right_whale.jpg','A Whale');
-/*!40000 ALTER TABLE `images` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `users`
 --
 
@@ -72,6 +44,34 @@ LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT INTO `users` VALUES (1,'eps','rarimar4@gmail.com','$2b$10$zzaew/e1pyNhMAtO6ZOeTu5EvwobdZg9ZRPeWNoo6K5zvlhkAqwjS','2023-06-15 11:49:32','2023-06-15 11:49:32'),(8,'test','rarimar@outlook.com','$2b$10$SAJivPAU7CUPrNkEDy2Z9.Rg2o1TC5JsLQ5aUhBemi/hJ8cOR6grm','2023-06-18 08:20:09','2023-06-18 08:20:09'),(14,'test1','rarimar1@outlook.com','$2b$10$WQSpxmuVYk/iKkSYXXdUfuf/WvwaaGY47vFnMFq8mKGo1e1ATq8U6','2023-06-20 02:00:53','2023-06-20 02:00:53');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `images` (
+  `imageid` int NOT NULL AUTO_INCREMENT,
+  `userid` int DEFAULT NULL,
+  `image_url` varchar(255) NOT NULL,
+  `image_description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`imageid`),
+  KEY `userid` (`userid`),
+  CONSTRAINT `images_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `images`
+--
+
+LOCK TABLES `images` WRITE;
+/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+INSERT INTO `images` VALUES (1,1,'https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQbxKU661kNI0m7Yw0xBW5SrN5Uc56_8cq4t4OhaYMXIvPhIgJo8v7H2d3PDtIDsfQt','A Girrafe'),(2,1,'https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRJpevBOQMgZ-Gaeb54y2Tin-o5vsoOtYW2nYtCpzkYqtj6ENWsHD4d1Bv-AUymOggA','A Lion'),(3,1,'https://upload.wikimedia.org/wikipedia/commons/3/3f/Walking_tiger_female.jpg','A Tiger'),(4,1,'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Southern_right_whale.jpg/220px-Southern_right_whale.jpg','A Whale'),(5,8,'https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRJpevBOQMgZ-Gaeb54y2Tin-o5vsoOtYW2nYtCpzkYqtj6ENWsHD4d1Bv-AUymOggA','User 8'),(6,8,'https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRJpevBOQMgZ-Gaeb54y2Tin-o5vsoOtYW2nYtCpzkYqtj6ENWsHD4d1Bv-AUymOggA','Id 8'),(7,8,'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Southern_right_whale.jpg/220px-Southern_right_whale.jpg','A Whale');
+/*!40000 ALTER TABLE `images` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
